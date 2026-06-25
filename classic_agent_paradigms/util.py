297d913366da53ee658b858f9d0c65aa09b058e5
EPIPHANY <tongyq2024@lzu.edu.cn>
2026-06-25 21:15:16 +0800

    react

--- classic_agent_paradigms/util.py
+++ classic_agent_paradigms/util.py
@@ -1,9 +1,9 @@
 import os
 from openai import OpenAI
-from dotenv import load_dotnev
+from dotenv import load_dotenv
 from typing import List, Dict
 
-load_dotnev()
+load_dotenv()
 
 class AgentsLLM:
     def __init__(self, model: str=None, apiKey: str=None, baseurl: str=None, timeout: int=None):
@@ -28,5 +28,34 @@ class AgentsLLM:
                 temperature=temperature,
                 stream=True,
                 )
-            
-    
+            print("LLM响应成功")
+            collected_content = []
+            for chunk in response:
+                if not chunk.choices:
+                    continue
+                content = chunk.choices[0].delta.content or ""
+                print(content, end="", flush=True)
+                collected_content.append(content)
+
+            print()
+            return "".join(collected_content)
+        except Exception as e:
+            print(f"调用LLM API时发生错误: {e}")
+            return None
+
+if __name__ == '__main__':
+    try:
+        llmClient = AgentsLLM()
+
+        exampleMessages = [
+            {"role": "sustem", "content": "You are a helpful assistant that writes Python code."},
+            {"role": "user", "content": "写一个快速排序算法"}
+            ]
+        print("--- 调用LLM ---")
+        responseText = llmClient.think(exampleMessages)
+        if responseText:
+            print("\n\n--- 完整模型响应 ---")
+            print(responseText)
+
+    except ValueError as e:
+        print(e)
